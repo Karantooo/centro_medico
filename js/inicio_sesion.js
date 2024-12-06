d61e4442dafc2647c1259f49ce4e7520fe9140cc
Nachopex <luisignaciomartinezneira@gmail.com>
2024-12-05 22:13:22 -0300

    feat: ahora se puede iniciar sesion

--- js/inicio_sesion.js
+++ js/inicio_sesion.js
@@ -6,27 +6,51 @@ form.addEventListener('submit', (event) => {
     acceder(); // Llama a tu función para manejar el inicio de sesión
 });
 
-function acceder(){
+async function acceder(){
     if(tipo_cuenta == "Médico"){
         const rut_medico = document.getElementById("RUT").value;
-        localStorage.setItem("rut_medico", rut_medico);
-        localStorage.removeItem("rut_paciente");
-        localStorage.removeItem("rut_admin");
-        window.location.href = '../website/agenda_medico.html'; //cambia al html de la info de los médicos
+        const contrasena = document.getElementById("contraseña").value;
+        const resultado = await verificar_datos_medico(rut_medico,contrasena);
+        if(resultado === 0){
+            alert("ACCEDIDO COMO MEDICO");
+            localStorage.setItem("rut_medico", rut_medico);
+            localStorage.removeItem("rut_paciente");
+            localStorage.removeItem("rut_admin");
+            window.location.href = '../website/agenda_medico.html'; //cambia al html de la info de los médicos
+        }
+        else{
+            alert("Intentelo denuevo");
+        }
     }
     else if(tipo_cuenta == "Paciente"){
         const rut_paciente = document.getElementById("RUT").value;
-        localStorage.setItem("rut_paciente", rut_paciente);
-        localStorage.removeItem("rut_medico");
-        localStorage.removeItem("rut_admin");
-        window.location.href = '../website/index.html'; //cambia al html de la info de los médicos
+        const contrasena = document.getElementById("contraseña").value;
+        const resultado = await verificar_datos_paciente(rut_paciente,contrasena);
+        if(resultado === 0){
+            localStorage.setItem("rut_paciente", rut_paciente);
+            localStorage.removeItem("rut_medico");
+            localStorage.removeItem("rut_admin");
+            window.location.href = '../website/index.html'; //cambia al html de la info de los médicos
+        }
+        else{
+            alert("Intentelo denuevo");
+        }
+
     }
     else if(tipo_cuenta == "Admin"){
         const rut_admin = document.getElementById("RUT").value;
-        localStorage.setItem("rut_admin", rut_admin);
-        localStorage.removeItem("rut_paciente");
-        localStorage.removeItem("rut_medico");
-        window.location.href = '../website/agenda_medico.html'; //cambia al html de la info de los médicos
+        const contrasena = document.getElementById("contraseña").value;
+        const resultado = await verificar_datos_admin(rut_admin,contrasena);
+        if(resultado === 0){
+            alert("ACCEDIDO COMO ADMIN");
+            localStorage.setItem("rut_admin", rut_admin);
+            localStorage.removeItem("rut_paciente");
+            localStorage.removeItem("rut_medico");
+            window.location.href = '../website/agenda_medico.html'; //cambia al html de la info de los médicos
+        }
+        else{
+            alert("Intentelo denuevo");
+        }
     }
 }
 
@@ -64,4 +88,76 @@ function cerrar_error(){
 
 function registrarse(){
     window.location.href = '../website/registrarse.html';
+}
+async function verificar_datos_paciente(Rut,Contra){
+    const usuario = {
+        rut: Rut,
+        contrasenia: Contra
+    };
+    try {
+        const response = await fetch("http://127.0.0.1:8000/usuario/paciente_login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+        if (response.status === 200) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    catch (error){
+        alert("Error");
+        return 1;
+    }
+}
+async function verificar_datos_medico(Rut,Contra){
+    const usuario = {
+        rut: Rut,
+        contrasenia: Contra
+    };
+    try {
+        const response = await fetch("http://127.0.0.1:8000/usuario/medico_login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+        if (response.status === 200) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    catch (error){
+        alert("Error");
+        return 1;
+    }
+}
+async function verificar_datos_admin(Rut,Contra){
+    const usuario = {
+        rut: Rut,
+        contrasenia: Contra
+    };
+    try {
+        const response = await fetch("http://127.0.0.1:8000/usuario/admin_login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+        if (response.status === 200) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    catch (error){
+        alert("Error");
+        return 1;
+    }
 }
